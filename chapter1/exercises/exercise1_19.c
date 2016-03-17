4bb37687f4caf9e7271118dda04f274a0eaa28dc
Yaroslav Sapozhnyk <yaroslav.sapozhnyk@globallogic.com>
2016-03-17 15:32:10 -0400

    Updated the reverse function according to the exercise

--- chapter1/exercises/exercise1_19.c
+++ chapter1/exercises/exercise1_19.c
@@ -2,46 +2,48 @@
 #define MAXLINE 1000 /* maximum input line length */
 
 int _getline(char line[], int maxline);
-void _reverse(char s[], int len);
+void _reverse(char s[]);
 
 /* Exercise 1-18. Write a program to remove trailing blanks and tabs from each line of input,
-and to delete entirely blank lines. */
-int main()
-{
-    int len; /* current line length */
-    char line[MAXLINE]; /* current input line */
+   and to delete entirely blank lines. */
+int main(){
+        int len; /* current line length */
+        char line[MAXLINE]; /* current input line */
 
-    while ((len = _getline(line, MAXLINE)) > 0){
-        _reverse(line, len);
-        printf("%s", line);
-    }
+        while ((len = _getline(line, MAXLINE)) > 0) {
+                _reverse(line);
+                printf("%s", line);
+        }
 
-    return 0;
+        return 0;
 }
 
-void _reverse(char s[], int len)
-{
-    char tmp;
-    if (s[len - 1] == '\n')
+void _reverse(char s[]){
+        int len = 0;
+        while (s[len] != '\0' && s[len] != '\n')
+                len++;
+
         len--;
-    for (int i = 0; i < len / 2; i++){
-        tmp = s[i];
-        s[i] = s[len - 1 - i];
-        s[len - 1 - i] = tmp;
-    }
+        char tmp;
+
+        for (int i = 0; i < len; i++){
+            tmp = s[i];
+            s[i] = s[len];
+            s[len] = tmp;
+            len--;
+        }
 }
 
 /* getline: read a line into s, return length */
-int _getline(char s[], int lim)
-{
-    int c, i;
+int _getline(char s[], int lim){
+        int c, i;
 
-    for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-        s[i] = c;
-    if (c == '\n') {
-        s[i] = c;
-        ++i;
-    }
-    s[i] = '\0';
-    return i;
+        for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+                s[i] = c;
+        if (c == '\n') {
+                s[i] = c;
+                ++i;
+        }
+        s[i] = '\0';
+        return i;
 }
