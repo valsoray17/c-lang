20f03c08574e0a31d6395f8edca29d1be1117b8a
Yaroslav Sapozhnyk <yaroslav.sapozhnik@gmail.com>
2016-03-18 20:00:15 -0400

    Formatted code

--- chapter1/exercises/exercise1_17_buffer.c
+++ chapter1/exercises/exercise1_17_buffer.c
@@ -10,37 +10,37 @@ void copy(char to[], char from[]);
 /* Exercise 1-17. Write a program to print all input lines that are longer than 80 characters. */
 int main()
 {
-        int len, c; /* current line length */
-        char line[FILTER]; /* current input line */
+	int len, c; /* current line length */
+	char line[FILTER]; /* current input line */
 
-        while ((len = getline1(line, FILTER)) > 0) {
-                if (is_whole_line(line, len, FILTER) == FALSE) {
-                        printf("%s", line);
-                        while ((c = getchar()) != EOF && c != '\n')
-                                putchar(c);
-                        if (c == '\n')
-                                putchar(c);
-                }
-        }
+	while ((len = getline1(line, FILTER)) > 0) {
+		if (is_whole_line(line, len, FILTER) == FALSE) {
+			printf("%s", line);
+			while ((c = getchar()) != EOF && c != '\n')
+				putchar(c);
+			if (c == '\n')
+				putchar(c);
+		}
+	}
 
-        return 0;
+	return 0;
 }
 
 int is_whole_line(char line[], int len, int size){
-        return (len < size - 1 || line[len - 1] == '\n') ? TRUE : FALSE;
+	return (len < size - 1 || line[len - 1] == '\n') ? TRUE : FALSE;
 }
 
 /* getline: read a line into s, return length */
 int getline1(char s[],int lim)
 {
-        int c, i;
+	int c, i;
 
-        for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
-                s[i] = c;
-        if (c == '\n') {
-                s[i] = c;
-                ++i;
-        }
-        s[i] = '\0';
-        return i;
+	for (i=0; i < lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+		s[i] = c;
+	if (c == '\n') {
+		s[i] = c;
+		++i;
+	}
+	s[i] = '\0';
+	return i;
 }
